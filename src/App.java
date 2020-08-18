723a4ac4b2e8429d03f07a2dbf17aa1ee27125bc
Kirill Orlov <8997919@gmail.com>
2020-08-18 09:32:58 +0300

    Black round pane added

--- src/App.java
+++ src/App.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -20,10 +21,15 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
-        Scene scene = new Scene(root);
+        root.setStyle("-fx-background-radius: 6;" +
+                "-fx-background-color: rgb(45, 45, 50), rgb(60, 60, 65);" +
+                "-fx-background-insets: 0, 0 1 1 0;");
+        Scene scene = new Scene(root, 420, 585, Color.TRANSPARENT);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.show();
+        // За верхнюю часть высотой 22 можно перемещать окно:
+        com.sun.glass.ui.Window.getWindows().get(0).setUndecoratedMoveRectangle(22);
     }
     @FXML
     void showDateTime(ActionEvent event) {
